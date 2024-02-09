4f80a335b09ab0cd7d9015e04786fcfa70efd96d
BenWhann <whann004@umn.edu>
2024-02-09 07:53:22 -0600

    refactoring was done

--- src/js/index.js
+++ src/js/index.js
@@ -13,71 +13,73 @@ const carbs = document.querySelector("#create-carbs");
 const protein = document.querySelector("#create-protein");
 const fat = document.querySelector("#create-fat");
 
+const displayEntry = (name, carbs, protein, fat) => {
+  list.insertAdjacentHTML(
+  "beforeend",
+  `<li class="card">
+      <div>
+        <h3 class="name">${capitalize(name)}</h3>
+        <div class="calories">${calculateCalories(
+          carbs,
+          protein,
+          fat
+        )} calories</div>
+        <ul class="macros">
+          <li class="carbs"><div>Carbs</div><div class="value">${
+            carbs
+          }g</div></li>
+          <li class="protein"><div>Protein</div><div class="value">${
+            protein
+          }g</div></li>
+          <li class="fat"><div>Fat</div><div class="value">${
+            fat
+          }g</div></li>
+        </ul>
+      </div>
+    </li>`
+);
+}
+
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+event.preventDefault();
 
-  API.post("/", {
-    fields: {
-      name: { stringValue: name.value },
-      carbs: { integerValue: carbs.value },
-      protein: { integerValue: protein.value },
-      fat: { integerValue: fat.value },
-    },
-  }).then((data) => {
-    console.log(data);
-    if (data.error) {
-        // there was an error
-        snackbar.show('Some data is missing here');
-        return;
-    }
+API.post("/", {
+fields: {
+  name: { stringValue: name.value },
+  carbs: { integerValue: carbs.value },
+  protein: { integerValue: protein.value },
+  fat: { integerValue: fat.value },
+},
+}).then((data) => {
+console.log(data);
+if (data.error) {
+  // there was an error
+  snackbar.show("Some data is missing.");
+  return;
+}
 
-    const capitalizedName = capitalize(name.value);
-    const totalCalories = calculateCalories(carbs.value, protein.value, fat.value);
-    snackbar.show('Food added successfully');
-    list.insertAdjacentHTML(
-        "beforeend",
-        `<li class="card">
-          <div>
-            <h3 class="name">${capitalizedName}</h3>
-            <div class="calories">${totalCalories} calories</div>
-            <ul class="macros">
-              <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
-              <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
-              <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
-            </ul>
-          </div>
-        </li>`
-    );
+snackbar.show("Food added successfully.");
 
-    name.value = "";
-    carbs.value = "";
-    protein.value = "";
-    fat.value = "";
-  });
+// TODO: refactor into displayEntry
+displayEntry(capitalize(name.value), carbs.value, protein.value, fat.value);
+
+name.value = "";
+carbs.value = "";
+protein.value = "";
+fat.value = "";
+});
 });
 
 const init = () => {
-  // TODO: Get the saved entries and list them
-API.get('').then(data => {
-  console.log(data.documents)
-  const documents = data.documents;
-  documents.forEach(document => {
-    console.log(document.fields.carbs)
-    list.insertAdjacentHTML(
-      "afterbegin",
-      `<li class="card">
-        <div>
-          <h3 class="name">${capitalize(document.fields.name.stringValue)}</h3>
-          <div class="calories">${calculateCalories(document.fields.carbs.integerValue, document.fields.protein.integerValue, document.fields.fat.integerValue)} calories</div>
-          <ul class="macros">
-            <li class="carbs"><div>Carbs</div><div class="value">${document.fields.carbs.integerValue}g</div></li>
-            <li class="protein"><div>Protein</div><div class="value">${document.fields.protein.integerValue}g</div></li>
-            <li class="fat"><div>Fat</div><div class="value">${document.fields.fat.integerValue}g</div></li>
-          </ul>
-        </div>
-      </li>`
-  )})
-})
+// the ?pageSize=100 is optional
+API.get("/?pageSize=100").then((data) => {
+  data.documents?.forEach((doc) => {
+    const fields = doc.fields;
+
+    // TODO: refactor into displayEntry
+    displayEntry(capitalize(fields.name.stringValue), fields.carbs.integerValue, fields.protein.integerValue, fields.fat.integerValue)
+  });
+});
 }
 
 init();
