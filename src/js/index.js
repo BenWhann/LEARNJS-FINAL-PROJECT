94fd12c969465ee75747bb369552929b2b61db0b
BenWhann <whann004@umn.edu>
2024-02-09 07:41:10 -0600

    list saved entries

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import FetchWrapper from "./helpers/fetch-wrapper.js";
 import { capitalize, calculateCalories } from './helpers/helpers.js'; 
-import snackbar from "snackbar";
+const snackbar = require('snackbar');
 
 const API = new FetchWrapper(
   "https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/beanwang"
@@ -55,3 +55,29 @@ form.addEventListener("submit", (event) => {
     fat.value = "";
   });
 });
+
+const init = () => {
+  // TODO: Get the saved entries and list them
+API.get('').then(data => {
+  console.log(data.documents)
+  const documents = data.documents;
+  documents.forEach(document => {
+    console.log(document.fields.carbs)
+    list.insertAdjacentHTML(
+      "afterbegin",
+      `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(document.fields.name.stringValue)}</h3>
+          <div class="calories">${calculateCalories(document.fields.carbs.integerValue, document.fields.protein.integerValue, document.fields.fat.integerValue)} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${document.fields.carbs.integerValue}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${document.fields.protein.integerValue}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${document.fields.fat.integerValue}g</div></li>
+          </ul>
+        </div>
+      </li>`
+  )})
+})
+}
+
+init();
